77d8ce81822780be99fa89469f5d86eedfd34bce
sriravic <srinath.pop@gmail.com>
2018-05-17 14:03:10 -0400

    some style changes and fixes to the npr integrator

--- src/nprintegrator.cpp
+++ src/nprintegrator.cpp
@@ -58,54 +58,57 @@ public:
                     // compute other terms and the final color
                     const float cosTheta = std::abs(Frame::cosTheta(wi));
                     Color3f L = Li * f * cosTheta * vis;
-                    
+
                     // Compute the edge color
-                    std::unique_ptr<Intersection> stencilHits(new Intersection[ray.m_totalStencilRays]);
-                    for (int rd = 0; rd < ray.m_totalStencilRays; rd++) {
-                        scene->rayIntersect(ray.getStencilRay(rd), stencilHits.get()[rd]);
+                    if (ray.m_hasRayDifferentials) {
+                        std::unique_ptr<Intersection> stencilHits(new Intersection[ray.m_totalStencilRays]);
+                        for (int rd = 0; rd < ray.m_totalStencilRays; rd++) {
+                            scene->rayIntersect(ray.getStencilRay(rd), stencilHits.get()[rd]);
+                        }
+
+                        // count m
+                        int m = 0;
+                        const Mesh* gS = its.mesh;
+                        for (int i = 0; i < ray.m_totalStencilRays; i++) {
+                            const Mesh* gR = stencilHits.get()[i].mesh;
+                            if (gR != gS)
+                                m++;
+                        }
+
+                        // check if m == 0
+                        // we can shade crease edges
+                        if (m == 0) {
+                            // m == 0 only when all the intersections are actually valid
+                            const Normal3f& n0 = stencilHits.get()[0].geoFrame.n;
+                            const Normal3f& n1 = stencilHits.get()[1].geoFrame.n;
+                            const Normal3f& n2 = stencilHits.get()[2].geoFrame.n;
+                            const Normal3f& n3 = stencilHits.get()[3].geoFrame.n;
+                            const Normal3f& n4 = stencilHits.get()[4].geoFrame.n;
+                            const Normal3f& n5 = stencilHits.get()[5].geoFrame.n;
+                            const Normal3f& n6 = stencilHits.get()[6].geoFrame.n;
+                            const Normal3f& n7 = stencilHits.get()[7].geoFrame.n;
+
+                            // front and sideways
+                            float dot1 = n0.dot(n4);
+                            float dot2 = n1.dot(n5);
+                            float dot3 = n2.dot(n6);
+                            float dot4 = n3.dot(n7);
+
+                            // check for creases
+                            if (abs(dot1) < m_threshCrease) m += 4;
+                            else if (abs(dot2) < m_threshCrease) m += 4;
+                            else if (abs(dot3) < m_threshCrease) m += 4;
+                            else if (abs(dot4) < m_threshCrease) m += 4;
+                        }
+
+                        // compute the edge strength metric
+                        const float factor = 0.5f * ray.m_totalStencilRays;
+                        const float edgeStrength = clamp(1.0f - std::abs(m - factor) / factor, 0.f, 1.f);
+
+                        // lerp between L and edge strength
+                        return (1.0f - edgeStrength) * L;
                     }
-
-                    // count m
-                    int m = 0;
-                    const Mesh* gS = its.mesh;
-                    for (int i = 0; i < ray.m_totalStencilRays; i++) {
-                        const Mesh* gR = stencilHits.get()[i].mesh;
-                        if (gR != gS)
-                            m++;
-                    }
-
-                    // check if m == 0
-                    // we can shade crease edges
-                    if (m == 0) {
-                        // we need the 0th, 2nd, 4th, 6th intersections and their surface normals
-                        const Normal3f& n0 = stencilHits.get()[0].geoFrame.n;
-                        const Normal3f& n1 = stencilHits.get()[1].geoFrame.n;
-                        const Normal3f& n2 = stencilHits.get()[2].geoFrame.n;
-                        const Normal3f& n3 = stencilHits.get()[3].geoFrame.n;
-                        const Normal3f& n4 = stencilHits.get()[4].geoFrame.n;
-                        const Normal3f& n5 = stencilHits.get()[5].geoFrame.n;
-                        const Normal3f& n6 = stencilHits.get()[6].geoFrame.n;
-                        const Normal3f& n7 = stencilHits.get()[7].geoFrame.n;
-                        // m == 0 only when all the intersections are actually valid
-
-                        // front and sideways
-                        float dot1 = n0.dot(n4);
-                        float dot2 = n1.dot(n5);
-                        float dot3 = n2.dot(n6);
-                        float dot4 = n3.dot(n7);
-                        
-                        if (abs(dot1) < m_threshCrease) m += 4;
-                        else if (abs(dot2) < m_threshCrease) m += 4;
-                        else if (abs(dot3) < m_threshCrease) m += 4;
-                        else if (abs(dot4) < m_threshCrease) m += 4;
-                    }
-
-                    // compute the edge metric
-                    float constant = 0.5f * ray.m_totalStencilRays;
-                    float edgeStrength = 1.0f - std::abs(m - constant) / constant;
-
-                    // lerp between L and edge strength
-                    return (1.0f - edgeStrength) * L + edgeStrength * Color3f(0.f);
+                    return L;
                 }
             }
             return Color3f(1.f);
